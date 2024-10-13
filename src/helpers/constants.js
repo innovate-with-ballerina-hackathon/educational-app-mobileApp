61111211e8f442162c5f5ec56a7f4541040ba023
KasuniWijeratne <kasuniwijeratne00@gmail.com>
2024-10-13 16:31:07 +0530

    adding gluestack ui

---
+++ src/helpers/constants.js
@@ -0,0 +1,6 @@
+import { Dimensions } from "react-native";
+
+export const HEIGHT = Dimensions.get("window").height;
+export const WIDTH = Dimensions.get("window").width;
+
+export const BACKEND_URL = "https://api.example.com";
