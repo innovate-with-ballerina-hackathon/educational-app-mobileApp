0c0bfec61adc63f6299d357abc5bfce05a71793a
KasuniWijeratne <kasuniwijeratne00@gmail.com>
2024-10-20 17:53:08 +0530

    diffrentiating tabs for tutor and student

--- src/navigation/topTabNavigator.js
+++ src/navigation/topTabNavigator.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { UserContext } from '../../App'; // Make sure the correct path to your UserContext is imported
 import HomeScreen from '../views/homeScreen';
 import UpcomingSessionsScreen from '../views/upcomingSession';
 import TutorsScreen from '../views/tutorScreen';
 import ChatScreen from '../views/chatScreen';
+import TutorUploadsScreen from '../views/tutorViews/fileUpload';
 
 const Tab = createMaterialTopTabNavigator();
 
 const TopTabNavigator = () => {
+  const { role } = useContext(UserContext); // Get the user role from context
+
   return (
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'gray',
-          tabBarIndicatorStyle: { backgroundColor: 'blue' },
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Upcoming Sessions" component={UpcomingSessionsScreen} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray',
+        tabBarIndicatorStyle: { backgroundColor: 'blue' },
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Upcoming Sessions" component={UpcomingSessionsScreen} />
+      {role === 'student' && (
         <Tab.Screen name="Tutors" component={TutorsScreen} />
-        <Tab.Screen name="Chat" component={ChatScreen} />
-      </Tab.Navigator>
+      )}
+      {role === 'tutor' && (
+        <Tab.Screen name="Tutor Uploads" component={TutorUploadsScreen} />
+      )}
+      <Tab.Screen name="Chat" component={ChatScreen} />
+    </Tab.Navigator>
   );
 };
 
