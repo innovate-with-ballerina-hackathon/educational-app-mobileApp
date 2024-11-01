df7a39ce8a969f300acc28fb74e2e6a0f04265e5
KasuniWijeratne <kasuniwijeratne00@gmail.com>
2024-11-01 13:50:31 +0530

    updated backend url

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -3,4 +3,4 @@ import { Dimensions } from "react-native";
 export const HEIGHT = Dimensions.get("window").height;
 export const WIDTH = Dimensions.get("window").width;
 
-export const BACKEND_URL = "https://api.example.com";
+export const BACKEND_URL = "http://localhost:9091";
